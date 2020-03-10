df059dd73570b0947fd533211738a90b43616053
Fariha Zainab <f.zainab.adm@gmail.com>
2020-03-09 22:58:10 -0700

    success

--- 5200_flask_app/app/api.py
+++ 5200_flask_app/app/api.py
@@ -2,49 +2,53 @@ from flask import Flask, request, render_template
 import os
 import json
 from flask.json import jsonify
-from .validations import validate_upload, validate_update
+from .validation import validate_upload, validate_update
 from . import app
 from werkzeug.datastructures import ImmutableDict
 
-@app.route('/api', methods=['GET'])
+@app.route('/api', methods-['GET'])
 def api():
     return render_template('whatever html needed')
 
 @app.route('/api/upload', methods=['POST'])
-def upload():
-    data = request.json #if posted using content type application/json then use request.get_json
-    
-    #pass data to validation module   TODO
+def api_upload():
+    data = request.form.to_dict()
+    #pass data to validation module
     #receive a message
-    
-    message=""
+    message = validate_upload(data)
+    #message=""
     if message == "Success":
         return jsonify(data)
     else:
-        return render_template('error.html', message = message), 400
+        errorData = {"message": message,
+                        "status": 400}
+        return jsonify(errorData)
 
 @app.route('/api/query', methods=['POST'])
 def query():
-    queriedData = request.json
-    
-    #pass data for validation     TODO
+    queriedData = request.form.to_dict()
+    #pass data for validation
     #receive message 
-    
-    message=""
+    message = validate_upload(queriedData)
     if message == "Success":
         return jsonify(queriedData)
     else:
-        return render_template('error.html', message = message), 400
+        errorData = {"message": message,
+                        "status": 400}
+        return jsonify(errorData)
 
 @app.route('/api/update/<addressID>', methods=['PUT'])
 def update():
-    updateData = request.json
-    addressID = updateData['addressID']
-    if addressID in checkAdressID: #a method in database to check if the adress id exists
+    updateData = request.form.to_dict()
+     #a method in database to check if the adress id exists
+    validationMessage = validate_update(updateData)
     
-        #send updateData for validation    TODO
-        #store data in database    TODO
-    
+    if validationMessage == "Success":
         return jsonify(updateData)
     else:
-        return render_template("error.html", message="Address ID not found!!!!")
+        errorData = {"message": validationMessage,
+                        "status": 400}
+        return jsonify(errorData)
+
+
+
